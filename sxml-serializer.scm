4508b523fe037f5b3c857a29ed7ea7abb87630aa
Jim Ursetto <zb@3e8.org>
2010-07-13 20:56:53 -0500

    get it to compile

--- sxml-serializer.scm
+++ sxml-serializer.scm
@@ -12,8 +12,17 @@
   )
 
 (import scheme chicken)
+(require-library srfi-1)
+(import (only srfi-1 filter))
 
 (include "serializer.scm")
 
+(define serialize-sxml srl:parameterizable)
+(define sxml->xml srl:sxml->xml)
+(define sxml->xml/noindent srl:sxml->xml-noindent)
+(define sxml->html srl:sxml->html)
+(define sxml->html/noindent srl:sxml->html-noindent)
+(define display-sxml srl:display-sxml)
+(define sxml->string srl:sxml->string)
 
 )
