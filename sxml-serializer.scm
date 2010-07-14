2deefa5f18c64193855aac087f20b54cde66dfe6
Jim Ursetto <zb@3e8.org>
2010-07-13 22:48:21 -0500

    expand and export conventional-ns-prefixes

--- sxml-serializer.scm
+++ sxml-serializer.scm
@@ -8,6 +8,8 @@
 
   display-sxml          ;; stupid name, it displays xml
   sxml->string
+
+  conventional-ns-prefixes
   
   )
 
@@ -24,5 +26,34 @@
 (define sxml->html/noindent srl:sxml->html-noindent)
 (define display-sxml srl:display-sxml)
 (define sxml->string srl:sxml->string)
+
+;; override srl:conventional-ns-prefixes so that sxml->xml etc. use the extended list
+(define srl:conventional-ns-prefixes
+  '((admin . "http://webns.net/mvcb/")
+    (atom . "http://www.w3.org/2005/Atom")
+    (cc . "http://web.resource.org/cc/")
+    (content . "http://purl.org/rss/1.0/modules/content/")
+    (dc . "http://purl.org/dc/elements/1.1/")
+    (feedburner . "http://rssnamespace.org/feedburner/ext/1.0")
+    (fo . "http://www.w3.org/1999/XSL/Format")
+    (geo . "http://www.w3.org/2003/01/geo/wgs84_pos#")
+    (georss . "http://www.georss.org/georss")
+    (itunes . "http://www.itunes.com/dtds/podcast-1.0.dtd")
+    (media . "http://search.yahoo.com/mrss/")
+    (rdf . "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+    (rng . "http://relaxng.org/ns/structure/1.0")
+    (rss . "http://purl.org/rss/1.0/") 
+    (slash . "http://purl.org/rss/1.0/modules/slash/")
+    (sy . "http://purl.org/rss/1.0/modules/syndication/")
+    (taxo . "http://purl.org/rss/1.0/modules/taxonomy/")
+    (wiki . "http://purl.org/rss/1.0/modules/wiki/")
+    (wfw . "http://wellformedweb.org/CommentAPI/")
+    (xlink . "http://www.w3.org/1999/xlink")
+    (xqx . "http://www.w3.org/2005/XQueryX")
+    (xsd . "http://www.w3.org/2001/XMLSchema")
+    (xsi . "http://www.w3.org/2001/XMLSchema-instance")
+    (xsl . "http://www.w3.org/1999/XSL/Transform")))
+
+(define conventional-ns-prefixes srl:conventional-ns-prefixes)
 
 )
